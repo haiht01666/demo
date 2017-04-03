80a49ca547cccf6bb69f2076fa932f9b2f0fd518
haiht01666 <haifpt2013@gmail.com>
2017-04-03 16:29:59 +0700

    update script db

--- DB/script.sql
+++ DB/script.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS DB_MANAGE_STAFF;
 
 USE DB_MANAGE_STAFF;
 
--- create table user s
+-- create table users
 CREATE TABLE DB_MANAGE_STAFF.users(
 	id integer auto_increment not null,
 	email varchar(50),
@@ -11,14 +11,16 @@ CREATE TABLE DB_MANAGE_STAFF.users(
 	name varchar(50),
 	phone varchar(50),
 	enable tinyint(1),
-    cdata datetime,
+    cdate datetime,
+	signup_date datetime,
     parent_id integer,
     bank_name varchar(50),
     bank_account varchar(50),
     bank_address varchar(50),
     city varchar(50),
+	child_id  varchar(1000),
     primary key(id)
-);
+)ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;
 
 -- create table roles
 CREATE TABLE DB_MANAGE_STAFF.roles(
@@ -27,7 +29,7 @@ CREATE TABLE DB_MANAGE_STAFF.roles(
     user_id integer,
     primary key(id),
     foreign key(user_id) references users(id)
-);
+)ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;
 
 -- create table order
 CREATE TABLE DB_MANAGE_STAFF.orders(
@@ -38,7 +40,7 @@ CREATE TABLE DB_MANAGE_STAFF.orders(
     user_id integer not null,
     primary key(id),
     foreign key(user_id) references users(id)
-);
+)ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;
 
 -- create table feedback
 CREATE TABLE DB_MANAGE_STAFF.feedbacks(
@@ -50,11 +52,11 @@ CREATE TABLE DB_MANAGE_STAFF.feedbacks(
     user_id integer not null,
     primary key(id),
     foreign key(user_id) references users(id)
-);
+)ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;
 
 -- set first value increment
 ALTER TABLE users AUTO_INCREMENT = 100000;
 
 -- Insert user 
-Insert into users(id,password,enable) values (100001,'123456',true);
+Insert into users(id,password,enable,name,child_id) values (100000,'$2a$10$DWjgKn32UUkM/ryxmnvCTOLvf.5EVLPuhOTVabdp3uMq49nOvhy7G',true,'ADMIN','100000');
 Insert into roles(id,role,user_id) values (1,'ADMIN',100000);
