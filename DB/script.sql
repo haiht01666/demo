a208bf13778b0ff5af98cc26b7bf05aa1b43378e
haiht01666 <haifpt2013@gmail.com>
2017-07-01 08:34:31 +0700

    update script

--- DB/script.sql
+++ DB/script.sql
@@ -1,10 +1,13 @@
 -- create database 
-CREATE DATABASE IF NOT EXISTS DB_MANAGE_STAFF;
+--CREATE DATABASE IF NOT EXISTS DB_MANAGE_STAFF;
 
-USE DB_MANAGE_STAFF;
+--USE DB_MANAGE_STAFF;
 
--- create table users
-CREATE TABLE DB_MANAGE_STAFF.users(
+-- ----------------------------
+--  Table structure for `users`
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(50) DEFAULT NULL,
   `password` varchar(100) DEFAULT NULL,
@@ -26,45 +29,102 @@ CREATE TABLE DB_MANAGE_STAFF.users(
   `address` varchar(100) DEFAULT NULL,
   `avatar` blob,
   PRIMARY KEY (`id`)
-)ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=100000 DEFAULT CHARSET=utf8;
 
--- create table roles
-CREATE TABLE DB_MANAGE_STAFF.roles(
-	id integer auto_increment not null,
-    role varchar(10),
-    user_id integer,
-    primary key(id),
-    foreign key(user_id) references users(id)
-)ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;
+DROP TABLE IF EXISTS `feedbacks`;
+CREATE TABLE `feedbacks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) DEFAULT NULL,
+  `cdate` datetime DEFAULT NULL,
+  `content` varchar(200) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `feedbacks_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
--- create table order
-CREATE TABLE DB_MANAGE_STAFF.orders(
-	id integer not null auto_increment,
-    name varchar(50),
-    cdate datetime,
-    user_id integer not null,
-	price double,
-	quantity integer,
-	type integer,
-	total double,
-    primary key(id),
-    foreign key(user_id) references users(id)
-)ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;
+-- ----------------------------
+--  Table structure for `orders`
+-- ----------------------------
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `cdate` datetime DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  `price` double DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `type` int(11) DEFAULT NULL,
+  `total` double DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
--- create table feedback
-CREATE TABLE DB_MANAGE_STAFF.feedbacks(
-	id integer not null auto_increment,
-    title varchar(50),
-    cdate datetime,
-    content varchar(200),
-    user_id integer not null,
-    primary key(id),
-    foreign key(user_id) references users(id)
-)ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;
+-- ----------------------------
+--  Table structure for `roles`
+-- ----------------------------
+DROP TABLE IF EXISTS `roles`;
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role` varchar(10) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `roles_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
+CREATE TABLE `articles` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) DEFAULT NULL,
+  `cdate` datetime DEFAULT NULL,
+  `content` varchar(200) DEFAULT NULL,
+  `sub_title` varchar(200) DEFAULT NULL,
+  `author` varchar(50) DEFAULT NULL,
+  `status` int,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT= 1 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `categories` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT= 1 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `products` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `description` varchar(1000) DEFAULT NULL,
+  `characteristic` varchar(10000) DEFAULT NULL,
+  `category_id` int DEFAULT NULL,
+  `image_url` varchar(200) DEFAULT NULL,
+  `price` double,
+  `cdate` datetime DEFAULT NULL,
+  `status` varchar(50) DEFAULT NULL,
+  `made_in` varchar(50) DEFAULT NULL,
+  `mail_product` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `products_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT= 1 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `revenues` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `cdate` datetime DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  `value` double DEFAULT NULL,
+  `type` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `revenues_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 -- set first value increment
 ALTER TABLE users AUTO_INCREMENT = 100000;
 
 -- Insert user 
 Insert into users(id,password,enable,name,child_id,cdate) values (100000,'$2a$10$DWjgKn32UUkM/ryxmnvCTOLvf.5EVLPuhOTVabdp3uMq49nOvhy7G',true,'SPADMIN','100000',now());
 Insert into roles(id,role,user_id) values (1,'SPADMIN',100000);
+
+-- Insert Category
+INSERT INTO categories(id,name) values (1,'Thời trang');
+INSERT INTO categories(id,name) values (2,'Mỹ phẩm');
